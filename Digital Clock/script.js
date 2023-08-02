ec8157bd650beecd1b333614a58ff69a6b8186c8
Willies <khanhduywin1907@gmail.com>
2023-08-02 21:37:28 +0700

    improve the digital clock a little bit

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,16 +1,27 @@
 //make the clock running
+const initTime = new Date();
+document.querySelector('.hours').innerHTML = displayTwoDigitNumber(initTime.getHours());
+document.querySelector('.minutes').innerHTML = displayTwoDigitNumber(initTime.getMinutes());
+document.querySelector('.seconds').innerHTML = displayTwoDigitNumber(initTime.getSeconds());
+document.querySelector('.date').innerHTML = ` ${initTime.toLocaleString('default', {
+	month: 'long',
+})} ${initTime.getDate()}, ${initTime.getFullYear()}`;
 
 const clockRunningInterval = setInterval(() => {
 	//Get time information
-	let currentDate = new Date();
-	let hour = currentDate.getHours();
-	let minute = currentDate.getMinutes();
-	let second = currentDate.getSeconds();
+	let currentDateTime = new Date();
 
 	//update on clock
-	document.querySelector('.hours').innerHTML = displayTwoDigitNumber(hour);
-	document.querySelector('.minutes').innerHTML = displayTwoDigitNumber(minute);
-	document.querySelector('.seconds').innerHTML = displayTwoDigitNumber(second);
+	document.querySelector('.hours').innerHTML = displayTwoDigitNumber(currentDateTime.getHours());
+	document.querySelector('.minutes').innerHTML = displayTwoDigitNumber(
+		currentDateTime.getMinutes()
+	);
+	document.querySelector('.seconds').innerHTML = displayTwoDigitNumber(
+		currentDateTime.getSeconds()
+	);
+	document.querySelector('.date').innerHTML = ` ${currentDateTime.toLocaleString('default', {
+		month: 'long',
+	})} ${currentDateTime.getDate()}, ${currentDateTime.getFullYear()}`;
 }, 1000);
 
 function displayTwoDigitNumber(number) {
