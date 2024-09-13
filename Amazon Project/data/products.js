abeaffd090ac07db51ee4c1f687888121f738ef4
Willies <khanhduywin1907@gmail.com>
2024-09-13 22:04:33 +0700

    use inheritance

--- Amazon Project/data/products.js
+++ Amazon Project/data/products.js
@@ -22,6 +22,27 @@ class Product {
 	getPrice() {
 		return `$${formatCurrency(this.priceCents)}`;
 	}
+
+	extraInfoHTML() {
+		return '';
+	}
+}
+
+class Clothing extends Product {
+	sizeChartLink;
+
+	constructor(productDetails) {
+		super(productDetails);
+		this.sizeChartLink = productDetails.sizeChartLink;
+	}
+
+	extraInfoHTML() {
+		return `
+			<a href="${this.sizeChartLink}" target='_blank'>
+				Size chart 
+			</a>
+		`;
+	}
 }
 
 // all products are converted to classes
@@ -519,5 +540,7 @@ export const products = [
 		keywords: ['umbrella', 'convenient', 'accessory', 'rain proof'],
 	},
 ].map((productDetails) => {
+	if (productDetails.type === 'clothing') return new Clothing(productDetails);
+
 	return new Product(productDetails);
 });
