e95781cb6c0e9e6b706866adcf284c7593cb3a43
Willies <khanhduywin1907@gmail.com>
2024-09-13 14:54:13 +0700

    Create class version of cart

--- Amazon Project/scripts/checkout.js
+++ Amazon Project/scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrders } from './checkout/orderSummary.js';
 import { renderPayment } from './checkout/payment.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrders();
 renderPayment();
